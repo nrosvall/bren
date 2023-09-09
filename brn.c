f4db7395c25830fbdea3220d45a5c8cc443f4725
Niko Rosvall <niko@byteptr.com>
2023-09-09 23:21:45 +0300

    Basic dir walking implemented

--- brn.c
+++ brn.c
@@ -21,14 +21,24 @@
  * brn should be fairly portable to any POSIX system.
  */
 
+#define _XOPEN_SOURCE 700
 #define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <glob.h>
+#include <string.h>
+#include <ftw.h>
 
 #define VERSION "0.4"
+
+typedef enum {
+	DEFAULT = 1,
+	ORIGINAL,
+	RANDOM,
+	SHA256
+	
+} Identifier_t;
 
 static void usage()
 {
@@ -42,7 +52,7 @@ OPTIONS\n\
     -b <name>          Set basename for the files\n\
     -o                 Use original filename as an identifier\n\
     -r                 Generate random, 8 characters long identifier\n\
-    -s                 Use SHA256 of the file as an identifier\n\
+    -s                 Use SHA256 of the file as a new name. Ignores -b\n\
 \n\
     -h                 Show short help and exit. This page\n\
     -V                 Show version number of the program\n\
@@ -59,22 +69,47 @@ AUTHORS\n\
 	printf(HELP);
 }
 
+static int do_work(const char *filepath, const struct stat *st,
+					int tflag, struct FTW *ftwbuffer) {
+
+	if (tflag == FTW_F) {
+		printf("%s\n", filepath);
+	}
+	
+	return 0;						
+}
+
+static void walk_path(const char *path, Identifier_t identifier,
+						 int fd_limit) {
+	int fflags = 0;
+
+	fflags |= FTW_DEPTH;
+	fflags |= FTW_PHYS;
+	
+	if (nftw(path, do_work, fd_limit, fflags) == -1) {
+		perror("nftw");
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main (int argc, char *argv[]) {
 
 	int index;
 	int c;
 	char *basename = NULL;
-	int oflag = 0;
-	int rflag = 0;
-	int sflag = 0;
+	char *path = NULL;
+	Identifier_t identifier = DEFAULT;
+	
+	int iflag_set = 0;
 
 	if (argc == 1) {
 		/* We don't have any arguments. Show usage and exit. */
 		usage();
 		return 0;
 	}
-	
+
+	path = argv[1];
+
 	while ((c = getopt(argc, argv, "b:horsV")) != -1) {
 		switch (c) {
 			case 'b': //basename
@@ -83,25 +118,36 @@ int main (int argc, char *argv[]) {
 				usage();
 				break;
 			case 'o': //use original name as id
-				oflag = 1;
+				if (iflag_set == 0) {
+					identifier = ORIGINAL;
+					iflag_set = 1;
+				}
 				break;
 			case 'r': //use generate random id (8 chars)
-				rflag = 1;
+				if (iflag_set == 0) {
+					identifier = RANDOM;
+					iflag_set = 1;
+				}
 				break;
-			case 's': //use sha 256 as an id
-				sflag = 1;
+			case 's': //use sha 256 as the new name, ignore b
+				if (iflag_set == 0) {
+					identifier = SHA256;
+					iflag_set = 1;
+				}
 				break;
 			case 'V':
 				printf("brn version %s\n", VERSION);
-				break;
+				return 0;
 			case '?':
 				usage();
 				return 0;
 		}
 	}
 
-	for (index = optind; index < argc; index++)
-		printf ("Non-option argument %s\n", argv[index]);
+	/*for (index = optind; index < argc; index++)
+		printf ("Non-option argument %s\n", argv[index]); */
+
+	walk_path(path, identifier, 15);
 
 	return 0;
 }
